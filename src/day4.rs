7109a023012723a8f93b575046de67a6b47b4a73
Kevin Brightwell <kevin.brightwell@faire.com>
2020-12-04 17:43:55 -0500

    Day 4 -- finally.

--- src/day4.rs
+++ src/day4.rs
@@ -1,6 +1,5 @@
 use std::hash::Hash;
 use std::cmp::PartialEq;
-use std::num::ParseIntError;
 use std::collections::HashSet;
 
 #[derive(Debug, Clone, PartialEq, Hash, Eq, Copy)]
@@ -16,12 +15,24 @@ pub enum PassportField {
     Unknown,
 }
 
+lazy_static! {
+    static ref VALID_COLORS: HashSet<&'static str> = {
+        let mut valid_colors = HashSet::with_capacity(7);
+        valid_colors.insert("amb");
+        valid_colors.insert("blu");
+        valid_colors.insert("brn");
+        valid_colors.insert("gry");
+        valid_colors.insert("grn");
+        valid_colors.insert("hzl");
+        valid_colors.insert("oth");
+
+        valid_colors
+    };
+}
+
 impl PassportField {
-    pub fn parse(str_pair: &str, validate: bool) -> Result<PassportField, (PassportField, String)> {
-        let pair = str_pair.split(PAIR_SEPARATOR).collect::<Vec<&str>>();
-        let key = pair[0];
-        
-        let field = match key {
+    pub fn parse(key: &str) -> Result<PassportField, String> {
+        match key {
             "byr" => Ok(PassportField::BirthYear),
             "iyr" => Ok(PassportField::IssueYear),
             "eyr" => Ok(PassportField::ExpirationYear),
@@ -30,19 +41,11 @@ impl PassportField {
             "ecl" => Ok(PassportField::EyeColor),
             "pid" => Ok(PassportField::PassportId),
             "cid" => Ok(PassportField::CountryId),
-            _ => Err((PassportField::Unknown, format!("Unknown key={:?}", key)))
-        };
-        
-        if validate {
-            let value = pair[1];
-            field?.validate(value)
-        }
-        else { 
-            field
+            _ => Err(format!("Unknown key={:?}", key)),
         }
     }
 
-    pub fn validate(&self, value: &str) -> Result<PassportField, (PassportField, String)> {
+    pub fn validate(&self, value: &str) -> Result<PassportField, String> {
         match self {
             &PassportField::BirthYear | &PassportField::IssueYear | &PassportField::ExpirationYear => {
                 let year = self.parse_u32(value)?;
@@ -58,10 +61,11 @@ impl PassportField {
                     return self.error(format!("Invalid year range value={:?} !in {:?}", year, range))
                 }
             },
+
             &PassportField::Height => {
                 let value_len = value.chars().count();
                 if value_len <= 2 {
-                    return self.error(format!("Invalid value={:?}", value))
+                    return self.malformed_input(value)
                 }
 
                 let cm_tag = value.chars().skip(value_len - 2).collect::<String>();
@@ -81,56 +85,81 @@ impl PassportField {
 
             &PassportField::HairColor => {
                 if value.chars().count() != 7 {
-                    return self.error(format!("Malformed input={:?}", value))
+                    return self.malformed_input(value)
                 }
 
                 let first_char = value.chars().next().unwrap();
                 if first_char != '#' {
-                    return self.error(format!("Malformed input={:?}", value))
+                    return self.malformed_input(value)
                 }
 
                 for c in value.chars().skip(1) {
                     if !((c >= '0' && c <= '9') || (c >= 'a' && c <= 'f')) {
-                        return self.error(format!("Malformed input={:?}", value))                        
+                        return self.malformed_input(value)                   
                     }
                 }
+            },
+
+            &PassportField::EyeColor => {
+                if value.chars().count() != 3 {
+                    return self.malformed_input(value)
+                }
+
+                if !VALID_COLORS.contains(value) {
+                    return self.error(format!("Unsupported eye color={:?}", value))
+                }
+            },
+
+            &PassportField::PassportId => {
+                if value.chars().count() != 9 {
+                    return self.malformed_input(value)
+                }
+
+                self.parse_u32(value)?;
             }
 
-            _ => todo!()
+            &PassportField::CountryId => (), // no validation!
+
+            &PassportField::Unknown => unreachable!(),
         }
 
         Ok(*self)
     }
 
-    fn error(&self, message: String) -> Result<PassportField, (PassportField, String)> {
-        Err((*self, message))
+    fn error(&self, message: String) -> Result<PassportField, String> {
+        Err(format!("{:?}: {}", self, message))
     }
 
-    fn parse_u32(&self, value: &str) -> Result<u32, (PassportField, String)> {
+    fn malformed_input(&self, value: &str) -> Result<PassportField, String> {
+        self.error(format!("Malformed input={:?}", value))
+    }
+
+    fn parse_u32(&self, value: &str) -> Result<u32, String> {
         value.parse::<u32>()
-            .map_err(|_| (*self, format!("Failed to parse u32 value={:?}", value)))
+            .map_err(|_| (format!("{:?}: Failed to parse u32 value={:?}", self, value)))
     }
 }
 
 const PAIR_SEPARATOR: char = ':';
 
 #[aoc_generator(day4)]
-pub fn input_generator(input: &str) -> Vec<HashSet<PassportField>> {
+pub fn input_generator(input: &str) -> Vec<Vec<(String, String)>> {
     let mut result = Vec::new();
-    let mut current_fields = HashSet::<PassportField>::new();
+
+    let mut current_fields = Vec::<(String, String)>::with_capacity(8);
     for line in input.lines() {
         if line.chars().count() == 0 {
             result.push(current_fields);
-            current_fields = HashSet::new();
+            current_fields = Vec::with_capacity(8);
         }
 
-        for field in line.split_whitespace().map(|pair| PassportField::parse(pair, false)) {
-            match field {
-                Ok(f) => {
-                    current_fields.insert(f);
-                },
-                Err((field, message)) => { println!("{:?} -> {:?}", field, message) },
-            };
+        let pairs = line.split_whitespace()
+            .map(|pair| {
+                pair.split(PAIR_SEPARATOR).map(str::to_string).collect::<Vec<String>>()
+            });
+
+        for pair in pairs {
+            current_fields.push((pair[0].clone(), pair[1].clone()));
         }
     }
 
@@ -139,78 +168,174 @@ pub fn input_generator(input: &str) -> Vec<HashSet<PassportField>> {
 }
 
 #[aoc(day4, part1)]
-pub fn solve_part1(passports: &[HashSet<PassportField>]) -> usize {
+pub fn solve_part1(passports: &[Vec<(String, String)>]) -> usize {
     passports.iter()
-    .filter(|&passport| {
-        let passport_len = passport.len();
-        passport_len == 8 || (passport_len == 7 && !passport.contains(&PassportField::CountryId))
-    })
-    .count()
+        .map(|fields| { 
+            fields.iter()
+                .map(|(field, _)| PassportField::parse(&field).unwrap())
+                .collect::<Vec<PassportField>>() 
+        })
+        .filter(|passport| {
+            let passport_len = passport.len();
+            if passport_len == 7 {
+                passport.iter().any(|f| f == &PassportField::CountryId)
+            }
+            else {
+                passport_len == 8
+            }
+        })
+        .count()
+}
+
+#[aoc(day4, part2)]
+pub fn solve_part2(passports: &[Vec<(String, String)>]) -> usize {
+    passports.iter()
+        .map(|fields| { 
+            fields.iter()
+                .map(|(key, value)| {
+                    let field = PassportField::parse(&key)?;
+                    field.validate(value)
+                })
+                .collect::<Vec<Result<PassportField, String>>>() 
+        })
+        .filter_map(|passport| {
+            let cleaned_fields = passport.into_iter()
+                .filter_map(|r| {
+                    match r {
+                        Ok(PassportField::CountryId) => None, // ignore
+
+                        Ok(f) => Some(f),
+
+                        Err(message) => { 
+                            println!("Failed to validate: {}", message); 
+                            None 
+                        }
+                    }
+                })
+                .collect::<HashSet<PassportField>>();
+
+            if cleaned_fields.len() == 7 { Some(cleaned_fields) } else { None }
+        })
+        .count()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    fn assert_validate_fail(field: PassportField, value: &str, error: &str) {
+        assert_eq!(field.validate(value), Err(error.to_string()));
+    }
+
+    fn assert_validate_success(field: PassportField, value: &str) {
+        assert_eq!(field.validate(value), Ok(field));
+    }
+
+    fn assert_parse_field(input: &str, field: PassportField) {
+        assert_eq!(PassportField::parse(input), Ok(field));
+    }
+
     #[test]
     fn passport_fields_height() {
-        assert_eq!(PassportField::parse("hgt:20", true), Err((PassportField::Height, String::from("Invalid value=\"20\""))));
-        assert_eq!(PassportField::parse("hgt:200", true), Err((PassportField::Height, String::from("Unknown unit value=\"200\""))));
-        assert_eq!(PassportField::parse("hgt:aaa", true), Err((PassportField::Height, String::from("Unknown unit value=\"aaa\""))));
-        assert_eq!(PassportField::parse("hgt:aaacm", true), Err((PassportField::Height, String::from("Failed to parse u32 value=\"aaa\""))));
-        
-        assert_eq!(PassportField::parse("hgt:200cm", true), Err((PassportField::Height, String::from("Invalid height=\"200cm\" !in 150..194cm"))));
-        assert_eq!(PassportField::parse("hgt:77in", true), Err((PassportField::Height, String::from("Invalid height=\"77in\" !in 59..77in"))));
+        assert_parse_field("hgt", PassportField::Height);
 
-        assert_eq!(PassportField::parse("hgt:150cm", true), Ok(PassportField::Height));
-        assert_eq!(PassportField::parse("hgt:193cm", true), Ok(PassportField::Height));
-        assert_eq!(PassportField::parse("hgt:59in", true), Ok(PassportField::Height));
-        assert_eq!(PassportField::parse("hgt:76in", true), Ok(PassportField::Height));
+        assert_validate_fail(PassportField::Height, "200", "Unknown unit value=\"200\"");
+        assert_validate_fail(PassportField::Height, "aaa", "Unknown unit value=\"aaa\"");
+        assert_validate_fail(PassportField::Height, "aaacm", "Failed to parse u32 value=\"aaa\"");
+        
+        assert_validate_fail(PassportField::Height, "200cm", "Invalid height=\"200cm\" !in 150..194cm");
+        assert_validate_fail(PassportField::Height, "77in", "Invalid height=\"77in\" !in 59..77in");
+
+        assert_validate_success(PassportField::Height, "150cm");
+        assert_validate_success(PassportField::Height, "193cm");
+        assert_validate_success(PassportField::Height, "59in");
+        assert_validate_success(PassportField::Height, "76in");
     }
 
     #[test]
     fn passport_fields_birthyear() {
-        assert_eq!(PassportField::parse("byr:aa", true), Err((PassportField::BirthYear, String::from("Failed to parse u32 value=\"aa\""))));
-        assert_eq!(PassportField::parse("byr:20", true), Err((PassportField::BirthYear, String::from("Invalid year range value=20 !in 1920..2003"))));
-        assert_eq!(PassportField::parse("byr:2003", true), Err((PassportField::BirthYear, String::from("Invalid year range value=2003 !in 1920..2003"))));
-        assert_eq!(PassportField::parse("byr:2020", true), Err((PassportField::BirthYear, String::from("Invalid year range value=2020 !in 1920..2003"))));
+        assert_parse_field("byr", PassportField::BirthYear);
 
-        assert_eq!(PassportField::parse("byr:1920", true), Ok(PassportField::BirthYear));
-        assert_eq!(PassportField::parse("byr:2002", true), Ok(PassportField::BirthYear));
+        assert_validate_fail(PassportField::BirthYear, "aa", "Failed to parse u32 value=\"aa\"");
+        assert_validate_fail(PassportField::BirthYear, "20", "Invalid year range value=20 !in 1920..2003");
+        assert_validate_fail(PassportField::BirthYear, "2003", "Invalid year range value=2003 !in 1920..2003");
+        assert_validate_fail(PassportField::BirthYear, "2020", "Invalid year range value=2020 !in 1920..2003");
+
+        assert_validate_success(PassportField::BirthYear, "1920");
+        assert_validate_success(PassportField::BirthYear, "2002");
     }
 
     #[test]
     fn passport_fields_issueyear() {
-        assert_eq!(PassportField::parse("iyr:aa", true), Err((PassportField::IssueYear, String::from("Failed to parse u32 value=\"aa\""))));
-        assert_eq!(PassportField::parse("iyr:20", true), Err((PassportField::IssueYear, String::from("Invalid year range value=20 !in 2010..2021"))));
-        assert_eq!(PassportField::parse("iyr:2009", true), Err((PassportField::IssueYear, String::from("Invalid year range value=2009 !in 2010..2021"))));
-        assert_eq!(PassportField::parse("iyr:2021", true), Err((PassportField::IssueYear, String::from("Invalid year range value=2021 !in 2010..2021"))));
+        assert_parse_field("iyr", PassportField::IssueYear);
 
-        assert_eq!(PassportField::parse("iyr:2010", true), Ok(PassportField::IssueYear));
-        assert_eq!(PassportField::parse("iyr:2020", true), Ok(PassportField::IssueYear));
+        assert_validate_fail(PassportField::IssueYear, "aa", "Failed to parse u32 value=\"aa\"");
+        assert_validate_fail(PassportField::IssueYear, "20", "Invalid year range value=20 !in 2010..2021");
+        assert_validate_fail(PassportField::IssueYear, "2009", "Invalid year range value=2009 !in 2010..2021");
+        assert_validate_fail(PassportField::IssueYear, "2021", "Invalid year range value=2021 !in 2010..2021");
+
+        assert_validate_success(PassportField::IssueYear, "2010");
+        assert_validate_success(PassportField::IssueYear, "2015");
+        assert_validate_success(PassportField::IssueYear, "2012");
+        assert_validate_success(PassportField::IssueYear, "2020");
     }
 
     #[test]
     fn passport_fields_expirationyear() {
-        assert_eq!(PassportField::parse("eyr:aa", true), Err((PassportField::ExpirationYear, String::from("Failed to parse u32 value=\"aa\""))));
-        assert_eq!(PassportField::parse("eyr:20", true), Err((PassportField::ExpirationYear, String::from("Invalid year range value=20 !in 2020..2031"))));
-        assert_eq!(PassportField::parse("eyr:2019", true), Err((PassportField::ExpirationYear, String::from("Invalid year range value=2019 !in 2020..2031"))));
-        assert_eq!(PassportField::parse("eyr:2031", true), Err((PassportField::ExpirationYear, String::from("Invalid year range value=2031 !in 2020..2031"))));
+        assert_parse_field("eyr", PassportField::ExpirationYear);
 
-        assert_eq!(PassportField::parse("eyr:2020", true), Ok(PassportField::ExpirationYear));
-        assert_eq!(PassportField::parse("eyr:2030", true), Ok(PassportField::ExpirationYear));
+        assert_validate_fail(PassportField::ExpirationYear, "aa", "Failed to parse u32 value=\"aa\"");
+        assert_validate_fail(PassportField::ExpirationYear, "20", "Invalid year range value=20 !in 2020..2031");
+        assert_validate_fail(PassportField::ExpirationYear, "2019", "Invalid year range value=2019 !in 2020..2031");
+        assert_validate_fail(PassportField::ExpirationYear, "2031", "Invalid year range value=2031 !in 2020..2031");
+
+        assert_validate_success(PassportField::ExpirationYear, "2020");
+        assert_validate_success(PassportField::ExpirationYear, "2030");
     }
 
     #[test]
     fn passport_fields_haircolor() {
-        assert_eq!(PassportField::parse("hcl:", true), Err((PassportField::HairColor, String::from("Malformed input=\"\""))));
-        assert_eq!(PassportField::parse("hcl:aaaaaa", true), Err((PassportField::HairColor, String::from("Malformed input=\"aaaaaa\""))));
-        assert_eq!(PassportField::parse("hcl:#", true), Err((PassportField::HairColor, String::from("Malformed input=\"#\""))));
-        assert_eq!(PassportField::parse("hcl:#a", true), Err((PassportField::HairColor, String::from("Malformed input=\"#a\""))));
-        assert_eq!(PassportField::parse("hcl:#aaaaa", true), Err((PassportField::HairColor, String::from("Malformed input=\"#aaaaa\""))));
-        assert_eq!(PassportField::parse("hcl:#aaaaaq", true), Err((PassportField::HairColor, String::from("Malformed input=\"#aaaaaq\""))));
+        assert_parse_field("hcl", PassportField::HairColor);
 
-        assert_eq!(PassportField::parse("hcl:#aaa000", true), Ok(PassportField::HairColor));
-        assert_eq!(PassportField::parse("hcl:#fff999", true), Ok(PassportField::HairColor));
+        assert_validate_fail(PassportField::HairColor, "", "Malformed input=\"\"");
+        assert_validate_fail(PassportField::HairColor, "aaaaaa", "Malformed input=\"aaaaaa\"");
+        assert_validate_fail(PassportField::HairColor, "#", "Malformed input=\"#\"");
+        assert_validate_fail(PassportField::HairColor, "#a", "Malformed input=\"#a\"");
+        assert_validate_fail(PassportField::HairColor, "#aaaaa", "Malformed input=\"#aaaaa\"");
+        assert_validate_fail(PassportField::HairColor, "#aaaaaq", "Malformed input=\"#aaaaaq\"");
+
+        assert_validate_success(PassportField::HairColor, "#aaa000");
+        assert_validate_success(PassportField::HairColor, "#fff999");
+    }
+
+    #[test]
+    fn passport_fields_eyecolor() {
+        assert_parse_field("ecl", PassportField::EyeColor);
+
+        assert_validate_fail(PassportField::EyeColor, "", "Malformed input=\"\"");
+        assert_validate_fail(PassportField::EyeColor, "hzlhzl", "Malformed input=\"hzlhzl\"");
+        assert_validate_fail(PassportField::EyeColor, "aa0", "Unsupported eye color=\"aa0\"");
+
+        for color in VALID_COLORS.iter() {
+            assert_validate_success(PassportField::EyeColor, color);
+        }
+    }
+
+    #[test]
+    fn passport_fields_passportid() {
+        assert_parse_field("pid", PassportField::PassportId);
+
+        assert_validate_fail(PassportField::PassportId, "", "Malformed input=\"\"");
+        assert_validate_fail(PassportField::PassportId, "000", "Malformed input=\"000\"");
+        assert_validate_fail(PassportField::PassportId, "0123456789", "Malformed input=\"0123456789\"");
+
+        assert_validate_fail(PassportField::PassportId, "aaaaaaaaa", "Failed to parse u32 value=\"aaaaaaaaa\"");
+
+        assert_validate_success(PassportField::PassportId, "000000001");
+        assert_validate_success(PassportField::PassportId, "012345678");
+        assert_validate_success(PassportField::PassportId, "087499704");
+        assert_validate_success(PassportField::PassportId, "896056539");
+        assert_validate_success(PassportField::PassportId, "545766238");
+        assert_validate_success(PassportField::PassportId, "093154719");
     }
 }
