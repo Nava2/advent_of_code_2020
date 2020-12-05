6b768884830c6fbb6b9840a471c687e83a544c84
Kevin Brightwell <kevin.brightwell@faire.com>
2020-12-05 12:55:19 -0500

    Day 5

--- src/day5.rs
+++ src/day5.rs
@@ -1,5 +1,7 @@
 use std::hash::Hash;
 use std::cmp::PartialEq;
+use bit_vec::BitVec;
+use std::ops::Range;
 
 #[derive(Debug, Clone, PartialEq, Hash, Eq, Copy)]
 pub enum Direction {
@@ -31,36 +33,121 @@ pub fn input_generator(input: &str) -> Vec<Vec<Direction>> {
 const ROW_PARTITION_COUNT: usize = 7;
 const ROW_COUNT: usize = 128;
 const COLUMN_COUNT: usize = 8;
+const TOTAL_VALUES: usize = ROW_COUNT * COLUMN_COUNT;
+
+pub fn calculate_seat_id(description: &[Direction]) -> usize {
+    let row = description.iter()
+        .take(ROW_PARTITION_COUNT)
+        .fold(0..ROW_COUNT, |current_row_range, partition| {
+            let partition_index = current_row_range.start + (current_row_range.end - current_row_range.start) / 2;
+            match partition {
+                Direction::Front => current_row_range.start..partition_index,
+                Direction::Back => partition_index..current_row_range.end,
+                _ => unreachable!(format!("Undefined partition={:?}", partition)),
+            }
+        }).start;
+    let column = description.iter()
+        .skip(ROW_PARTITION_COUNT)
+        .fold(0..COLUMN_COUNT, |current_column_range, partition| {
+            let partition_index = current_column_range.start + (current_column_range.end - current_column_range.start) / 2;
+            match partition {
+                Direction::Left => current_column_range.start..partition_index,
+                Direction::Right => partition_index..current_column_range.end,
+                _ => unreachable!(format!("Undefined partition={:?}", partition)),
+            }
+        }).start;
+    
+    // seat_id
+    row * COLUMN_COUNT + column
+}
+
+fn id_at(row: usize, column: usize) -> usize {
+    row * COLUMN_COUNT + column
+}
+
+fn id_to(id: &usize) -> (usize, usize) {
+    (*id / COLUMN_COUNT, *id % COLUMN_COUNT)
+}
 
 #[aoc(day5, part1)]
 pub fn solve_part1(seat_descriptions: &[Vec<Direction>]) -> usize {
     seat_descriptions.iter()
-        .map(|desc| { 
-            let row = desc.iter()
-                .take(ROW_PARTITION_COUNT)
-                .fold(0..ROW_COUNT, |current_row_range, partition| {
-                    let partition_index = current_row_range.start + (current_row_range.end - current_row_range.start) / 2;
-                    match partition {
-                        Direction::Front => current_row_range.start..partition_index,
-                        Direction::Back => partition_index..current_row_range.end,
-                        _ => unreachable!(format!("Undefined partition={:?}", partition)),
-                    }
-                }).start;
-            let column = desc.iter()
-                .skip(ROW_PARTITION_COUNT)
-                .fold(0..COLUMN_COUNT, |current_column_range, partition| {
-                    let partition_index = current_column_range.start + (current_column_range.end - current_column_range.start) / 2;
-                    match partition {
-                        Direction::Left => current_column_range.start..partition_index,
-                        Direction::Right => partition_index..current_column_range.end,
-                        _ => unreachable!(format!("Undefined partition={:?}", partition)),
-                    }
-                }).start;
-            (row, column)
-        })
-        .map(|(row, column)| row * COLUMN_COUNT + column) // seat_id
+        .map(|desc| calculate_seat_id(&desc))
         .max()
         .unwrap()
+}
+
+fn load_id_map_with_only_valid_values(marked_ranges: &[Range<usize>]) -> BitVec {
+    let mut invalid_markers = BitVec::with_capacity(TOTAL_VALUES);
+
+    for i in 0..id_at(ROW_COUNT - 1, COLUMN_COUNT) {
+        invalid_markers.push(marked_ranges.iter().any(|r| r.contains(&i)))
+    }
+
+    invalid_markers
+}
+
+fn print_table(markers: &BitVec) {
+    if !cfg!(debug_assertions) {
+        return;
+    }
+
+    for (id, v) in markers.iter().enumerate() {
+        let (row, column) = id_to(&id);
+        if column == 0 {
+            if id > 0 {
+                println!("|");
+            }
+
+            print!("R{:>03} |", row);
+        }
+
+        print!("{}", v as i32);
+    }
+
+    println!("!");
+}
+
+#[aoc(day5, part2)]
+pub fn solve_part2(seat_descriptions: &[Vec<Direction>]) -> usize {
+    let valid_values = id_at(1, 0)..id_at(ROW_COUNT - 1, COLUMN_COUNT);
+
+    let given_ids = seat_descriptions.iter()
+        .filter_map(|desc| {
+            let id = calculate_seat_id(&desc);
+            if valid_values.contains(&id) { Some(id) } else { None }
+        });
+
+
+    let invalid_ranges = vec![
+        0..COLUMN_COUNT,
+        id_at(ROW_COUNT - 1, 0)..id_at(ROW_COUNT - 1, COLUMN_COUNT)
+    ];
+
+    let mut invalid_markers = load_id_map_with_only_valid_values(&invalid_ranges);
+
+    for given_id in given_ids {
+        invalid_markers.set(given_id, true);
+    }
+
+    print_table(&invalid_markers);
+    
+    let result = invalid_markers.iter()
+        .enumerate()
+        .filter_map(|(i, v)| if !v { Some(i) } else { None })
+        .find(|check_idx| {
+            invalid_markers[check_idx - 1] && invalid_markers[check_idx + 1]
+        }) // -1 / +1 are set
+        .unwrap();
+
+    print_table(&invalid_markers);
+
+    if cfg!(debug_assertions) {
+        let (row, column) = id_to(&result);
+        println!("row={}, column={}, id={}", row, column, result);
+    }
+
+    result
 }
 
 #[cfg(test)]
