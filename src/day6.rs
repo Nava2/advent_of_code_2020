f128bb1d95bface12376963b31f39868392f5c6c
Kevin Brightwell <kevin.brightwell@faire.com>
2020-12-06 09:16:36 -0500

    Day 6 part 1

--- src/day6.rs
+++ src/day6.rs
@@ -1,13 +1,28 @@
-use std::hash::Hash;
-use std::cmp::PartialEq;
-use bit_vec::BitVec;
-use std::ops::Range;
+use std::collections::HashSet;
 
 #[aoc_generator(day6)]
 pub fn input_generator(input: &str) -> Vec<Vec<char>> {
-    input.lines()
-        .map(|line| line.chars().collect::<Vec<char>>())
-        .collect::<Vec<Vec<char>>>()
+    let mut result = Vec::<Vec<char>>::new();
+
+    let mut current_group = Vec::<char>::new();
+    for line in input.lines() {
+        if line.chars().count() == 0 {
+            result.push(current_group);
+            current_group = Vec::new();
+        }
+
+        line.chars().for_each(|c| { current_group.push(c); });
+    }
+
+    result.push(current_group);
+    result
+}
+
+#[aoc(day6, part1)]
+pub fn solve_part1(groups: &[Vec<char>]) -> usize {
+    groups.iter()
+        .map(|g| g.iter().cloned().collect::<HashSet<char>>().len())
+        .sum()
 }
 
 #[cfg(test)]
@@ -16,8 +31,13 @@ mod tests {
 
     #[test]
     fn input_parsing() {
-        assert_eq!(input_generator("abc")[0], vec![
-            'a', 'b', 'c'
-        ]);
+        assert_eq!(
+            input_generator("abc")[0], vec![
+                'a', 'b', 'c'
+            ].into_iter().collect::<HashSet<char>>());
+        assert_eq!(
+            input_generator("ac\nb\n\n")[0], vec![
+                'a', 'b', 'c'
+            ].into_iter().collect::<HashSet<char>>());
     }
 }
