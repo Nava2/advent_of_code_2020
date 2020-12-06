0459e4fd04ecfd24c6e4301ed40ff087a9f45883
Kevin Brightwell <kevin.brightwell@faire.com>
2020-12-06 10:44:54 -0500

    Day 6 P2

--- src/day6.rs
+++ src/day6.rs
@@ -9,6 +9,7 @@ pub fn input_generator(input: &str) -> Vec<Vec<Vec<char>>> {
         if line.chars().count() == 0 {
             result.push(current_group);
             current_group = Vec::new();
+            continue;
         }
 
         current_group.push(line.chars().collect());
@@ -18,16 +19,39 @@ pub fn input_generator(input: &str) -> Vec<Vec<Vec<char>>> {
     result
 }
 
+fn collect_set(input: &[char]) -> HashSet<char> {
+    input.iter().cloned().collect()
+}
+
 #[aoc(day6, part1)]
 pub fn solve_part1(groups: &[Vec<Vec<char>>]) -> usize {
     groups.iter()
-        .map(|g| g.iter().cloned().flatten().collect::<HashSet<char>>().len())
+        .map(|g| g.iter().cloned().flatten().collect::<HashSet<char>>())
+        .map(|common_answers| common_answers.len())
         .sum()
 }
 
 #[aoc(day6, part2)]
 pub fn solve_part2(groups: &[Vec<Vec<char>>]) -> usize {
-    panic!()
+    let all_chars_set = ('a'..'z').collect::<HashSet<char>>();
+    groups.iter()
+        .map(|group| {
+            let group = group.iter()
+                .map(|v| collect_set(v))
+                .collect::<Vec<HashSet<char>>>();
+
+            // seed with first
+            group
+                .iter()
+                .skip(1)
+                .fold(group[0].clone(), |group_set, person| {
+                    group_set.intersection(&person)
+                        .cloned()
+                        .collect::<HashSet<char>>()
+                })
+        })
+        .map(|shared| shared.len())
+        .sum()
 }
 
 #[cfg(test)]
