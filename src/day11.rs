cf611ae1e1db39e6cb232d65da9d0ee91c3cb381
Kevin Brightwell <kevin.brightwell@faire.com>
2020-12-12 13:45:12 -0500

    Day 11 P2 + Cleanup

--- src/day11.rs
+++ src/day11.rs
@@ -93,7 +93,60 @@ fn count_occupied(layout: &[Vec<Space>], i: usize, j: usize) -> usize {
     count
 }
 
-fn iterate_occupied(layout: &[Vec<Space>]) -> (Vec<Vec<Space>>, bool){
+fn count_occupied_sight(layout: &[Vec<Space>], i: usize, j: usize) -> usize {
+    // [i - 1, j - 1][i - 1, j][i - 1, j + 1]
+    // [    i, j - 1][    i, j][    i, j + 1]
+    // [i + 1, j - 1][i + 1, j][i + 1, j + 1]
+    
+    let mut count = 0;
+
+    fn check_diag(layout: &[Vec<Space>], row_len: &usize, i: &usize, j: &usize, walk: (i32, i32)) -> bool {
+        let mut i = *i as i64 + walk.0 as i64;
+        let mut j = *j as i64 + walk.1 as i64;
+
+        loop {
+            if i < 0 || j < 0 {
+                return false
+            }
+
+            let ui = i as usize;
+            let uj = j as usize;
+
+            if ui >= layout.len() || uj >= *row_len {
+                return false
+            }
+
+            match &layout[i as usize][j as usize] {
+                Space::Occupied => return true,
+                Space::Empty => return false,
+                _ => {}
+            }
+
+            i += walk.0 as i64;
+            j += walk.1 as i64;
+        }
+    }
+
+    let row_len = layout[0].len();
+
+    for i_offset in -1..=1 {
+        for j_offset in -1..=1 {
+            if i_offset == 0 && j_offset == 0 {
+                continue;
+            }
+
+            if check_diag(&layout, &row_len, &i, &j, (i_offset, j_offset)) {
+                count += 1;
+            }
+        }
+    }
+
+    count
+}
+
+type CountOccupiedFn = dyn Fn(&[Vec<Space>], usize, usize) -> usize;
+
+fn iterate_occupied(layout: &[Vec<Space>], count_occupied: &CountOccupiedFn, occupied_req: usize) -> (Vec<Vec<Space>>, bool){
     let mut mutated = false;
     let new_layout = layout.iter()
         .enumerate()
@@ -102,7 +155,7 @@ fn iterate_occupied(layout: &[Vec<Space>]) -> (Vec<Vec<Space>>, bool){
                 .enumerate()
                 .map(|(j, seat)| match seat {
                     Space::Occupied => {
-                        if count_occupied(layout, i, j) >= 4 {
+                        if count_occupied(layout, i, j) >= occupied_req {
                             mutated = true;
                             Space::Empty
                         }
@@ -128,35 +181,29 @@ fn iterate_occupied(layout: &[Vec<Space>]) -> (Vec<Vec<Space>>, bool){
     (new_layout, mutated)
 }
 
-#[aoc(day11, part1)]
-pub fn solve_part1(layout: &[Vec<Space>]) -> usize {
+fn solve(layout: &[Vec<Space>], count_occupied: &CountOccupiedFn, occupied_req: usize) -> usize {
     let mut mutated = true;
     let mut new_layout = layout.to_vec();
-    print_layout(&new_layout);
 
-    let mut iter_count = 0;
     while mutated {
-        // println!("Iter={}", iter_count);
-        let result = iterate_occupied(&new_layout);
+        let result = iterate_occupied(&new_layout, count_occupied, occupied_req);
         mutated = result.1;
         new_layout = result.0;
-
-        print_layout(&new_layout);
-
-        iter_count += 1;
-        // println!();
     }
 
-    return new_layout.into_iter()
+    new_layout.into_iter()
         .flat_map(|r| r.into_iter().filter(|s| *s == Space::Occupied))
-        .count();
+        .count()
+}
 
-    fn print_layout(layout: &[Vec<Space>]) {
-        // for row in layout {
-        //     println!("{}", row.iter().map(|s| format!("{}", s)).collect::<Vec<_>>().join(""));
-        // }
-        // println!();
-    }
+#[aoc(day11, part1)]
+pub fn solve_part1(layout: &[Vec<Space>]) -> usize {
+    solve(layout, &count_occupied, 4)
+}
+
+#[aoc(day11, part2)]
+pub fn solve_part2(layout: &[Vec<Space>]) -> usize {
+    solve(&layout, &count_occupied_sight, 5)
 }
 
 #[cfg(test)]
@@ -201,5 +248,63 @@ mod tests {
                 Space::Empty, 
             ]
         );
+    }
+
+    #[test]
+    fn check_sight_count_given() {
+        let layout = input_generator(
+            ".......#.\n\
+             ...#.....\n\
+             .#.......\n\
+             .........\n\
+             ..#L....#\n\
+             ....#....\n\
+             .........\n\
+             #........\n\
+             ...#.....");
+
+        assert_eq!(8, count_occupied_sight(&layout, 4, 3));
+    }
+
+    #[test]
+    fn check_sight_count_given_2() {
+        let layout = input_generator(
+            ".............\n\
+             .L.L.#.#.#.#.\n\
+             .............");
+
+        assert_eq!(0, count_occupied_sight(&layout, 1, 1));
+        assert_eq!(1, count_occupied_sight(&layout, 1, 3));
+    }
+
+    #[test]
+    fn check_sight_count_given_3() {
+        let layout = input_generator(
+            ".##.##.\n\
+             #.#.#.#\n\
+             ##...##\n\
+             ...L...\n\
+             ##...##\n\
+             #.#.#.#\n\
+             .##.##.");
+
+        assert_eq!(0, count_occupied_sight(&layout, 3, 3));
+    }
+
+    #[test]
+    fn solve_part2_given_1() {
+        let layout = input_generator(
+            "L.LL.LL.LL\n\
+             LLLLLLL.LL\n\
+             L.L.L..L..\n\
+             LLLL.LL.LL\n\
+             L.LL.LL.LL\n\
+             L.LLLLL.LL\n\
+             ..L.L.....\n\
+             LLLLLLLLLL\n\
+             L.LLLLLL.L\n\
+             L.LLLLL.LL");
+
+        assert_eq!(26, solve_part2(&layout));
     }
 }
