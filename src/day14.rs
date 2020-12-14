7f67a61eb124303c8d69dbaf4d3e360b9b10d70f
Kevin Brightwell <kevin.brightwell@faire.com>
2020-12-14 10:14:00 -0500

    Day 14 prep

---
+++ src/day14.rs
@@ -0,0 +1,14 @@
+#[aoc_generator(day14)]
+pub fn input_generator(input: &str) -> Vec<u32> {
+    panic!()
+}
+
+#[aoc(day14, part1)]
+pub fn solve_part1(bus_notes: &[u32]) -> u32 {
+    panic!()
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+}
