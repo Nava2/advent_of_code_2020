82e068b5b905b5d0edda470eff6a2ec33c94794c
Kevin Brightwell <kevin.brightwell@faire.com>
2020-12-15 09:22:47 -0500

    Day 14 P2

--- src/day14.rs
+++ src/day14.rs
@@ -1,31 +1,49 @@
 use regex::Regex;
 use std::collections::HashMap;
+use bit_vec::BitVec;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum MaskBit {
+    One,
+    Zero,
+    DontCare,
+}
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct Mask {
-    and_mask: u64,
-    or_mask: u64,
+    bits: Vec<MaskBit>,
 }
 
 impl Mask {
-    fn parse(mask: &str) -> Option<Mask> {
+    fn parse(mask: &str) -> Mask {
+        Mask {
+            bits: mask.chars().map(|c| match c {
+                    '1' => MaskBit::One,
+                    '0' => MaskBit::Zero,
+                    'X' => MaskBit::DontCare,
+                    _ => panic!(),
+                })
+                .collect()
+        }
+    }
+
+    fn load_mask(&self) -> (u64, u64) {
         let mut and_mask = 0;
         let mut or_mask = 0;
-        for c in mask.chars() {
-            match c {
-                '1' => { 
+        for c in &self.bits {
+            match &c {
+                MaskBit::One => { 
                     or_mask |= 1; 
                     and_mask |= 1; 
                 },
-                '0' => { 
+                MaskBit::Zero => { 
                     or_mask |= 0; 
                     and_mask |= 0; 
                 },
-                'X' => {
+                MaskBit::DontCare => {
                     or_mask |= 0;
                     and_mask |= 1;
                 }
-                _ => return None,
             };
 
             or_mask <<= 1;
@@ -35,27 +53,73 @@ impl Mask {
         or_mask >>= 1;
         and_mask >>= 1;
 
-        Some(
-            Mask {
-                and_mask,
-                or_mask,
-            }
-        )
+        (and_mask, or_mask)
     }
 
-    fn mask(&self, value: u64) -> u64 {
-        (value & self.and_mask) | self.or_mask
+    fn apply_mask(value: u64, mask: &(u64, u64)) -> u64 {
+        let (and_mask, or_mask) = mask;
+        (value & and_mask) | or_mask
+    }
+
+    fn all_addresses_iter(&self, address: &usize) -> impl Iterator<Item = usize> {
+        fn _generate<'a, I>(address: &usize, bit_width: usize, bit_no: usize, mut generator: I, address_accum: &mut BitVec<u32>) -> Box<dyn Iterator<Item = usize>>
+            where
+                I: Iterator<Item = &'a MaskBit> + Clone,
+        {            
+            if let Some(mask_bit) = generator.next() {
+                let b = (address & (1 << (bit_no - 1))) >> (bit_no - 1);
+                let b = b == 1;
+
+                match mask_bit {
+                    MaskBit::Zero => { // unchanged
+                        address_accum.push(b);
+                        _generate(address, bit_width, bit_no - 1, generator, address_accum)
+                    },
+                    MaskBit::One => { // push 1
+                        address_accum.push(true);
+                        _generate(address, bit_width, bit_no - 1, generator, address_accum)
+                    },
+                    MaskBit::DontCare => { // split and generate both
+                        let first = {
+                            let mut address_accum = address_accum.clone();
+                            address_accum.push(true);
+
+                            let generator = generator.clone();
+                            _generate(address, bit_width, bit_no - 1, generator, &mut address_accum)
+                        };
+
+                        let second = {
+                            // don't need to clone this time
+                            address_accum.push(false); 
+                            _generate(address, bit_width, bit_no - 1, generator, address_accum)
+                        };
+
+                        Box::new(first.chain(second))
+                    }
+                }
+            } else {
+                // finished 
+                let result = address_accum.iter().fold(0 as usize, |a, v| {
+                    (a << 1) | (if v { 1 } else { 0 })
+                });
+                Box::new(std::iter::once(result))
+            }
+        }
+
+        let bit_width = self.bits.len();
+        let mut accum = BitVec::with_capacity(bit_width);
+        _generate(&address, bit_width, bit_width, self.bits.iter(), &mut accum)
     }
 }
 
 trait Maskable {
-    fn mask(&self, mask: &Option<&Mask>) -> u64;
+    fn mask(&self, mask: &Option<(u64, u64)>) -> u64;
 }
 
 impl Maskable for u64 {
-    fn mask(&self, mask: &Option<&Mask>) -> u64 {
+    fn mask(&self, mask: &Option<(u64, u64)>) -> u64 {
         if let Some(mask) = mask {
-            mask.mask(*self)
+            Mask::apply_mask(*self, mask)
         }
         else {
             *self
@@ -87,7 +151,7 @@ impl Instruction {
         }
         else if let Some(cp) = MASK_RE.captures(line) {
             let mask = cp.name("mask")?.as_str();
-            Instruction::Mask(Mask::parse(mask)?)
+            Instruction::Mask(Mask::parse(mask))
         }
         else {
             panic!()
@@ -108,14 +172,36 @@ pub fn input_generator(input: &str) -> Vec<Instruction> {
 #[aoc(day14, part1)]
 pub fn solve_part1(program: &[Instruction]) -> u64 {
     let mut memory = HashMap::<usize, u64>::new();
+    let mut mask: Option<(u64, u64)> = None;
+
+    for ins in program {
+        match ins {
+            Instruction::Mask(m) => { mask = Some(m.load_mask()); },
+            Instruction::Write(address, value) => { 
+                let entry = memory.entry(*address).or_insert(0);
+                *entry = value.mask(&mask); 
+            },
+        }
+    }
+
+    memory.values().fold(0u64, |a, v| a + *v)
+}
+
+#[aoc(day14, part2)]
+pub fn solve_part2(program: &[Instruction]) -> u64 {
+    let mut memory = HashMap::<usize, u64>::new();
     let mut mask: Option<&Mask> = None;
 
     for ins in program {
         match ins {
-            Instruction::Mask(m) => { mask = Some(m); },
+            Instruction::Mask(m) => { mask = Some(&m); },
             Instruction::Write(address, value) => { 
-                let entry = memory.entry(*address).or_insert(0);
-                *entry = value.mask(&mask); 
+                if let Some(mask) = mask {
+                    for gen_address in mask.all_addresses_iter(address) {
+                        let entry = memory.entry(gen_address).or_insert(0);
+                        *entry = *value; 
+                    }
+                }
             },
         }
     }
@@ -141,6 +227,33 @@ mod tests {
     }
 
     #[test]
+    fn solve_part2_given1() {
+        let program = input_generator(
+            "mask = 000000000000000000000000000000X1001X\n\
+             mem[42] = 100\n\
+             mask = 00000000000000000000000000000000X0XX\n\
+             mem[26] = 1"
+        );
+
+        assert_eq!(solve_part2(&program), 208);
+    }
+
+    #[test]
+    fn generate_addresses_given1() {
+        let mask = Mask::parse("000000000000000000000000000000X1001X");
+        let address = 42;
+
+        // 000000000000000000000000000000011010  (decimal 26)
+        // 000000000000000000000000000000011011  (decimal 27)
+        // 000000000000000000000000000000111010  (decimal 58)
+        // 000000000000000000000000000000111011  (decimal 59)
+        assert_eq!(
+            mask.all_addresses_iter(&address).collect::<Vec<_>>(),
+            vec![59, 58, 27, 26],
+        )
+    }
+
+    #[test]
     fn input_generator_given1() {
         let program = input_generator(GIVEN_INPUT1);
 
@@ -149,9 +262,12 @@ mod tests {
             vec![
                 Instruction::Mask(
                     Mask { 
-                        // mask =   XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X
-                        and_mask: 0b111111111111111111111111111111111101,
-                        or_mask:                               0b1000000,
+                        bits: std::iter::repeat(MaskBit::DontCare).take(29)
+                            .chain(std::iter::once(MaskBit::One))
+                            .chain(std::iter::repeat(MaskBit::DontCare).take(4))
+                            .chain(std::iter::once(MaskBit::Zero))
+                            .chain(std::iter::once(MaskBit::DontCare))
+                            .collect::<Vec<_>>(),
                     }
                 ),
                 Instruction::Write(8, 11),
