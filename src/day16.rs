915dff37a54c9d0d8441483dd3d412662b1af185
Kevin Brightwell <kevin.brightwell@faire.com>
2020-12-17 09:00:45 -0500

    Add missing days and inputs

---
+++ src/day16.rs
@@ -0,0 +1,14 @@
+#[aoc_generator(day16)]
+pub fn input_generator(input: &str) -> Vec<u32> {
+    panic!()
+}
+
+#[aoc(day16, part1)]
+pub fn solve_part1(program: &[u32]) -> u64 {
+    panic!()
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+}
---
+++ src/day17.rs
@@ -0,0 +1,14 @@
+#[aoc_generator(day17)]
+pub fn input_generator(input: &str) -> Vec<u32> {
+    panic!()
+}
+
+#[aoc(day17, part1)]
+pub fn solve_part1(program: &[u32]) -> u64 {
+    panic!()
+}
+
+#[cfg(test)]
+mod tests {
+    // use super::*;
+}
